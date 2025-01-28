a4b07e0823d105690808872a1009aa6e20b17d72
nathanachinger <nathan.achinger@ufl.edu>
2025-01-27 19:12:21 -0500

    RFD900x test code

--- Core/Src/user.cpp
+++ Core/Src/user.cpp
@@ -3,22 +3,56 @@
 #include "RFD900x.hpp"
 #include "main.h"
 
+#define START_CHAR 0xFF
+#define ESC_CHAR 0x2F
+#define END_CHAR 0x3F
+
 // Main C++ loop
 void user_loop_cpp()
 {
 	RFD900x rfd900x(&huart1);
 	Notecard notecard(&huart2);
 
-	HAL_Delay(4000); // Must give notecard time to setup
+	uint8_t test_packet[] = {START_CHAR, 'h', 'e', 'l', 'l', 'o', '\0', END_CHAR};
 
+	while(1) {
+		HAL_UART_Transmit(&huart1, test_packet, sizeof(test_packet)/sizeof(test_packet[0]), HAL_MAX_DELAY);
+		HAL_Delay(500);
+	}
+
+
+	// Create a test CAN frame
+	uint8_t data[] = {0x19, 0x39, 0x5, 0x19};
+	TESTFrame frame;
+	frame.can_id = 0x000045E2;
+	memcpy(frame.data, data, sizeof(data));
+	frame.len = 4;
+
+	// Must give notecard time to setup
+	HAL_Delay(4000);
+
+	// Setup connection to Notehub
 	uint8_t setup[] = "{\"req\":\"hub.set\",\"product\":\"edu.ufl.nathan.achinger:flare_telemetry\",\"mode\":\"continuous\"}\n";
 	HAL_UART_Transmit(&huart2, setup, sizeof(setup)/sizeof(setup[0]), HAL_MAX_DELAY);
 
 	HAL_Delay(200);
 
-	//uint8_t packet[] = "{\"req\":\"card.status\"}";
-	uint8_t packet[] = "{\"req\":\"note.add\",\"body\":{\"param1\":200,\"param2\":400}}\n";
-	HAL_UART_Transmit(&huart2, packet, sizeof(packet)/sizeof(packet[0]), HAL_MAX_DELAY);
+	uint8_t json_packet[100];
+	sprintf((char*)json_packet, "{\"req\":\"note.add\",\"body\":{\"%32lu\":{\"len\":%u}}\n", frame.can_id, frame.len);
+	HAL_UART_Transmit(&huart2, json_packet, sizeof(json_packet)/sizeof(json_packet[0]), HAL_MAX_DELAY);
+
+	//   \"body\":{\"CAN_ID\":{\"len\":\"0x00\",\"DATA\":\"data\"}}\n;
+
+	/* body:{
+	 *   0xid:{
+	 *      len: 0x00,
+	 *      data: [...]
+	 *   }
+	 * }
+	 *
+	 *
+	 *
+	 */
 
 	uint8_t rx_buff[50];
 	HAL_UART_Receive(&huart2, rx_buff, 5, 1000);
